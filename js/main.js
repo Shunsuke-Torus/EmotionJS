d270d3c5942ba79a26de11cec95211972fd097c4
Shunsuke-Torus <shunsuke.takasu@gmail.com>
2022-03-03 11:25:41 +0900

    複数人でも感情のグラフを描画できるようにした。

--- js/main.js
+++ js/main.js
@@ -30,7 +30,7 @@ function copy_to_blob(){
 function transfer(blob){
     let subscriptionKey = "";
 
-    let uriBase ="";
+    let uriBase ="https://~~~.azure.com/face/v1.0/detect";
 
     // Request parameters.
     let params = {
@@ -89,47 +89,67 @@ function line(data_json){//複数人
 }
 
 function emotion(data_json){//1人
-    let emotion_data =(data_json[0].faceAttributes.emotion);
-    let anger = JSON.stringify(emotion_data.anger);
-    let contempt = JSON.stringify(emotion_data.contempt);
-    let disgust = JSON.stringify(emotion_data.disgust);
-    let fear = JSON.stringify(emotion_data.fear);
-    let happiness = JSON.stringify(emotion_data.happiness);
-    let neutral = JSON.stringify(emotion_data.neutral);
-    let sadness = JSON.stringify(emotion_data.sadness);
-    let surprise = JSON.stringify(emotion_data.surprise);
-    //複数回撮影するとcanvasに重ねて描画される。解決:canvasの初期化
-    let canvas = document.getElementById("myChart");
-    let ctx = document.getElementById("myChart").getContext("2d");
-    let myChart = new Chart(ctx,{
-        type: "pie",
-        data: {
-            labels: ["怒りanger", "軽蔑contempt", "嫌悪disgust", "恐れfear",
-                        "幸せhappiness", "自然neutral","悲しみsadness","驚きsurprise"],    
-            datasets: [{
-                data: [anger, contempt,disgust,fear,
-                        happiness,neutral,sadness,surprise]
-            }]
-        },
-        options: {
-            responsive:true,
-            title:{
-                display:true,
-                text : "結果",
-                fontSize:15
-            },
-            legend: {
-                display:true,
-                position: "bottom",
-                fontSize:5,
-                stepSize:5
-            },
-            plugins:{
-                colorschemes:{
-                    scheme: 'brewer.Paired12'
-                }
-            }
+    //複数回撮影するとcanvasに重ねて描画される。解決:id(myChart_s)の中の要素の初期化
+    let parent = document.getElementById("myChart_s");
+    let judge = !!document.getElementById("myChart_s")
+    if(true==judge){
+        parent.innerHTML = ''; //子要素を全て削除
+    };
+    for(let i=0; i<data_json.length; i++){
+        let emotion_data =(data_json[i].faceAttributes.emotion);
+        let anger = JSON.stringify(emotion_data.anger);
+        let contempt = JSON.stringify(emotion_data.contempt);
+        let disgust = JSON.stringify(emotion_data.disgust);
+        let fear = JSON.stringify(emotion_data.fear);
+        let happiness = JSON.stringify(emotion_data.happiness);
+        let neutral = JSON.stringify(emotion_data.neutral);
+        let sadness = JSON.stringify(emotion_data.sadness);
+        let surprise = JSON.stringify(emotion_data.surprise);
+        
+        let count = i + 1;
+        let str ="";
 
-        }
-    });
+        $("#myChart_s").append("<div class = 'del' class=chart>",
+                                "<p>"+ count +"<p>",
+                                "<canvas id='myChart"+i+"' class='emo_chart' style='position: relative; height:480px; width:640px' ></canvas>",
+                                "</div>"
+                                );
+
+        $("myChart"+i).ready(function() {
+            let canvas = document.getElementById("myChart"+i);
+            var ctx = document.getElementById("myChart"+i).getContext("2d");
+            console.log(ctx);
+            let myChart = new Chart(ctx,{
+                type: "pie",
+                data: {
+                    labels: ["怒りanger", "軽蔑contempt", "嫌悪disgust", "恐れfear",
+                                "幸せhappiness", "自然neutral","悲しみsadness","驚きsurprise"],    
+                    datasets: [{
+                        data: [anger, contempt,disgust,fear,
+                                happiness,neutral,sadness,surprise]
+                    }]
+                },
+                options: {
+                    responsive:false,
+                    title:{
+                        display:true,
+                        text : "結果",
+                        fontSize:15
+                    },
+                    legend: {
+                        display:true,
+                        position: "bottom",
+                        fontSize:5,
+                        stepSize:5
+                    },
+                    plugins:{
+                        colorschemes:{
+                            scheme: 'brewer.Paired12'
+                        }
+                    }
+        
+                }
+            });
+        });
+    }
 }
